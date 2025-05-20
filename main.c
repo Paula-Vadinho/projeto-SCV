b9eff67b4259823147666cbee397cf7b9931f4d3
Tuturr-C <arthurgerotto2007@gmail.com>
2025-05-20 14:43:08 -0300

    Codigo novo organizado

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define nome 49
+#define Nnome 49
 
+struct produto
+{
+    char nome [Nnome];
+    int codigo; 
+	int Qestoque;
+    float Pvenda;
+	int Cvenda;
+	float Vtotal;
+}
 int main()
 {
-    char nome [nome];
-    int codigo, Qestoque, Cvenda;
-    float Pvenda, Vtotal;
-
 	printf("\n\t\t\t Bem vindo a loja de Informatica");
 	printf("digite o produto");
-
+	
 
 	system("pause");
 	return 0;
