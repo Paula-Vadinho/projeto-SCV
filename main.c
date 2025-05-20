a5b3edf43636a194e45f70ca2e7086fbf8122dd6
Tuturr-C <arthurgerotto2007@gmail.com>
2025-05-20 14:15:50 -0300

    Arumando o começo

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main()
 	float produto, codp, nomp, qntp, prp;
 	float venda, codv, prodv, vt;
 	printf("\n\t\t\t Bem vindo a loja de Informatica");
+	printf("digite o produto");
 
 
 	system("pause");
