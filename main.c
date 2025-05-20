b1f1d5fe6612be749a6ad222e2a51eb25a82812a
MtBalbine <matheusbalbine@gmail.com>
2025-05-20 15:36:48 -0300

    Base da atividade

--- main.c
+++ main.c
@@ -5,17 +5,51 @@
 struct produto
 {
     char nome [Nnome];
-    int codigo; 
+    int codigo;
 	int Qestoque;
     float Pvenda;
 	int Cvenda;
 	float Vtotal;
+};
+
+int leitura (int item)
+{
+
+
+    return 0;
 }
+
 int main()
 {
-	printf("\n\t\t\t Bem vindo a loja de Informatica");
-	printf("digite o produto");
-	
+    int i, opcao=0;
+
+    do {
+
+        printf("\n\n\t\tBEM VINDO!!");
+        printf("\n\t1 - Cadastrar um Produto");
+        printf("\n\t2 - Registrar uma Venda");
+        printf("\n\t3 - Lista de Produtos Disponíveis");
+        printf("\n\t4 - Lista de Vendas");
+        printf("\n\t5 - Relatório");
+        printf("\n\t6 - Fechar o programa");
+
+        printf("\n\n\t\tEscolha uma das opcoes: ");
+        scanf("%d", &opcao);
+
+        switch(opcao)
+        {
+        case 1:
+
+
+
+
+        }
+
+
+
+
+    }while (opcao!=6);
+
 
 	system("pause");
 	return 0;
