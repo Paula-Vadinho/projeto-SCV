46ba91f39530d3d704af185b3e66aa1b5206522e
Tuturr-C <arthurgerotto2007@gmail.com>
2025-05-20 14:10:38 -0300

    Deixai bonitinho

--- main.c
+++ main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-	int produto, cod, nom, qnt;
-
+	float produto, codp, nomp, qntp, prp;
+	float venda, codv, prodv, vt;
+	printf("\n\t\t\t Bem vindo a loja de Informatica");
+	
+	
 	system("pause");
 	return 0;
 
