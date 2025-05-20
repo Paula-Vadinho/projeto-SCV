1547c761562391117dbfa61225f5378a34dafcdc
MtBalbine <matheusbalbine@gmail.com>
2025-05-20 15:33:15 -0300

    Base da atividade

--- main.c
+++ main.c
@@ -1,8 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+<<<<<<< Updated upstream
 int main()
 {
     printf("vc é legal");
     return 0;
+=======
+struct produto
+{
+    char nome [Nnome];
+    int codigo;
+	int Qestoque;
+    float Pvenda;
+	int Cvenda;
+	float Vtotal;
+};
+
+int leitura (int item)
+{
+
+
+    return 0;
+}
+
+int main()
+{
+    int i, opcao=0;
+
+    do {
+
+        printf("\n\n\t\tBEM VINDO!!");
+        printf("\n\t1 - Cadastrar um Produto");
+        printf("\n\t2 - Registrar uma Venda");
+        printf("\n\t3 - Lista de Produtos Disponíveis");
+        printf("\n\t4 - Lista de Vendas");
+        printf("\n\t5 - Relatório");
+        printf("\n\t6 - Fechar o programa");
+
+        printf("\n\n\t\tEscolha uma das opcoes: ");
+        scanf("%d", &opcao);
+
+        switch(opcao)
+        {
+        case 1:
+
+
+
+
+        }
+
+
+
+
+    }while (opcao!=6);
+
+
+	system("pause");
+	return 0;
+>>>>>>> Stashed changes
 }
