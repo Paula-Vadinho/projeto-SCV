d6a1401c6a5fae3b380b1b785f45c627850db5a0
MtBalbine <matheusbalbine@gmail.com>
2025-05-20 14:14:14 -0300

    Negocinho

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
 	float produto, codp, nomp, qntp, prp;
 	float venda, codv, prodv, vt;
 	printf("\n\t\t\t Bem vindo a loja de Informatica");
-	
-	
+
+
 	system("pause");
 	return 0;
 
